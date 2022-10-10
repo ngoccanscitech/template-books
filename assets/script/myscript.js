dce3bbec6d080d0cc20d441b5297c251c2ab9968
cannn <ngoccanscitech@gmail.com>
2022-10-11 06:23:15 +0700

    init project too long

---
+++ assets/script/myscript.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#carousel-example-one').carousel({
+		interval: false,
+		keyboard: false,
+		pause: "hover"
+	});
+	$('#carousel-example-two').carousel({
+		interval: false,
+		keyboard: false,
+		pause: "hover"
+	});
+	$('#carousel-example-three').carousel({
+		interval: false,
+		keyboard: false,
+		pause: "hover"
+	});
+	$('#carousel-example-four').carousel({
+		interval: false,
+		keyboard: false,
+		pause: "hover"
+	});
+	$('#carousel-example-fifth').carousel({
+		interval: false,
+		keyboard: false,
+		pause: "hover"
+	});
+})
